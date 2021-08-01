9f10059e0a36d4f94a496660f7649cbc37cb7deb
Guy Segal <guysegal.il@gmail.com>
2021-08-01 17:47:53 +0300

    remove types from package.json

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,4 @@ export * from './withRetry';
 export * from './withThrow';
 export * from './withAuthorize';
 export * from './authErrors';
-export { FetchEnhancer, Fetch, EnhancedFetchRequestInit } from './types';
+export * from './types';
