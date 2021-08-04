514166bfca80f467155c4a3cc7457975bb195a91
Guy Segal <guysegal.il@gmail.com>
2021-08-04 13:25:01 +0300

    added missing export

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export * from './enhancers/withBasicAuth';
 export * from './enhancers/withRetry';
 export * from './enhancers/withThrow';
 export * from './enhancers/withTimeout';
+export * from './enhancers/withHeaders';
 export * from './types';
