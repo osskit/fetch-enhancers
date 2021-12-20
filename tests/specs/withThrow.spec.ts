a39176940f13a1606026506d8c47ecab14d1dc5a
Idan Goor <idangoor1@gmail.com>
2021-12-20 18:08:54 +0200

    rollback

--- tests/specs/withThrow.spec.ts
+++ tests/specs/withThrow.spec.ts
@@ -1,6 +1,6 @@
 import { createServer } from 'http';
 import { AddressInfo } from 'net';
-import fetch  from 'node-fetch';
+import fetch from 'node-fetch';
 
 import { withThrow, FetchError } from '../../src';
 
@@ -8,8 +8,12 @@ const throwingFetch = withThrow(fetch);
 
 test('throws error when fetch fails ', async () => {
   const server = createServer((_, res) => {
-    res.writeHead(500);
-    res.end();
+    const serverError = { type: 'mySpecialError' };
+
+    res.writeHead(400, {
+      'Content-Type': 'application/json',
+    });
+    res.end(JSON.stringify(serverError));
   });
 
   return new Promise<void>((resolve, reject) => {
@@ -18,8 +22,9 @@ test('throws error when fetch fails ', async () => {
       try {
         await throwingFetch(`http://127.0.0.1:${port}`);
         reject();
-      } catch (err) {
+      } catch (err: unknown) {
         expect(err).toBeInstanceOf(FetchError);
+        expect((err as FetchError).data?.type).toBe('mySpecialError');
         resolve();
       } finally {
         server.close();
