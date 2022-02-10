2bb86c26fa9a0cacd0e0578f44255cffe53c8481
Guy Segal <guysegal.il@gmail.com>
2022-02-10 15:05:54 +0200

    change withHeaders to accept dynamic getHeaders function

--- src/enhancers/withHeaders.ts
+++ src/enhancers/withHeaders.ts
@@ -3,12 +3,12 @@ import type { RequestInfo, RequestInit } from 'node-fetch';
 import type { Fetch } from '../types';
 
 export const withHeaders =
-  (fetch: Fetch, headers: Record<string, string>): Fetch =>
+  (fetch: Fetch, getHeaders: () => Record<string, string>): Fetch =>
   (url: RequestInfo, init?: RequestInit) =>
     fetch(url, {
       ...init,
       headers: {
         ...init?.headers,
-        ...headers,
+        ...getHeaders(),
       },
     });
