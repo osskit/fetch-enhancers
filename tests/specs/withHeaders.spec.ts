13b700b52b87286198db3e2ddc5ce95c8eac9e5a
Guy Segal <guysegal.il@gmail.com>
2022-02-10 16:19:17 +0200

    added test

---
+++ tests/specs/withHeaders.spec.ts
@@ -0,0 +1,31 @@
+import { createServer } from 'http';
+import { AddressInfo } from 'net';
+import fetch from 'node-fetch';
+
+import {withHeaders } from '../../src';
+
+const fetchWithHeaders = withHeaders(fetch, () => ({"foo": "bar"}));
+
+test('throws error when fetch fails ', async () => {
+  const server = createServer((req, res) => {
+    const response = req.headers['foo']
+
+    res.writeHead(200)
+    res.end(response);
+  });
+
+  return new Promise<void>((resolve, reject) => {
+    server.listen(async () => {
+      const { port } = server.address() as AddressInfo;
+      try {
+        const res = await fetchWithHeaders(`http://127.0.0.1:${port}`);
+        expect(await res.text()).toBe('bar');
+        resolve();
+      } finally {
+        server.close();
+      }
+    });
+    server.on('error', reject);
+  });
+});
+
