72e1faf1e9d2e0be7ef421cfddab00c7b2ad8b09
Sharon Grossman <sharong1337@gmail.com>
2022-03-07 16:40:32 +0200

    pass existing headers to getHeaders

--- src/enhancers/withHeaders.ts
+++ src/enhancers/withHeaders.ts
@@ -1,14 +1,14 @@
-import type { RequestInfo, RequestInit } from 'node-fetch';
+import type { HeadersInit, RequestInfo, RequestInit } from 'node-fetch';
 
 import type { Fetch } from '../types';
 
 export const withHeaders =
-  (fetch: Fetch, getHeaders: () => Record<string, string>): Fetch =>
+  (fetch: Fetch, getHeaders: (headers?: HeadersInit) => Record<string, string>): Fetch =>
   (url: RequestInfo, init?: RequestInit) =>
     fetch(url, {
       ...init,
       headers: {
         ...init?.headers,
-        ...getHeaders(),
+        ...getHeaders(init?.headers),
       },
     });
