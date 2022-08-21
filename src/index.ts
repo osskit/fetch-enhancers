2e8621056f45f3438f9b2242a6c7681ca11e309a
Noam Okman <noamokman@gmail.com>
2022-08-21 15:24:30 +0300

    fix error export (#13)

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,5 @@ export * from './enhancers/withThrow.js';
 export * from './enhancers/withTimeout.js';
 export * from './enhancers/withHeaders.js';
 export * from './enhancers/withToken.js';
+export * from './fetchError.js';
 export * from './types.js';
