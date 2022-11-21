e8a3a2dd5ffefd637daf6d2d6086377d797ab6c7
Daniel Witz <danielwitz@gmail.com>
2022-11-21 15:00:31 +0200

    pass 504 status code on timeout and the options on the error data

--- tests/specs/withTimeout.spec.ts
+++ tests/specs/withTimeout.spec.ts
@@ -4,7 +4,8 @@ import fetch from 'node-fetch';
 
 import { withTimeout, FetchError } from '../../src/index.js';
 
-const timeoutFetch = withTimeout(fetch, { requestTimeoutMs: 100 });
+const timeoutOptions = { requestTimeoutMs: 100 };
+const timeoutFetch = withTimeout(fetch, timeoutOptions);
 
 describe('withTimeout', () => {
   it('single request configuration - times out when server does not respond in time', async () => {
@@ -24,33 +25,8 @@ describe('withTimeout', () => {
             reject(new Error('should not get here'));
           } catch (error) {
             expect(error).toBeInstanceOf(FetchError);
-            resolve();
-          } finally {
-            server.close();
-          }
-        });
-        server.on('error', reject);
-      }),
-    ).resolves.toBeUndefined();
-  });
-
-  it('global configuration - times out when server does not respond in time', async () => {
-    const server = createServer((_, res) => {
-      setTimeout(() => {
-        res.writeHead(200);
-        res.end();
-      }, 2000);
-    });
-
-    await expect(
-      new Promise<void>((resolve, reject) => {
-        server.listen(async () => {
-          const { port } = server.address() as AddressInfo;
-          try {
-            await timeoutFetch(`http://127.0.0.1:${port}`);
-            reject(new Error('should not get here'));
-          } catch (error) {
-            expect(error).toBeInstanceOf(FetchError);
+            expect((error as FetchError).status).toBe(504);
+            expect((error as FetchError).data).toStrictEqual({ timeoutOptions });
             resolve();
           } finally {
             server.close();
