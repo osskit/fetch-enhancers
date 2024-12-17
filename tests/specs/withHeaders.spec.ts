97cb6f666470baa28bd1a4839cff5a3e728b7b4a
Guy Segal <guysegal.il@gmail.com>
2024-12-17 15:38:17 +0200

    withHeaders - validate in test that headers are being added on top of the existing headers (#27)

--- tests/specs/withHeaders.spec.ts
+++ tests/specs/withHeaders.spec.ts
@@ -23,11 +23,11 @@ describe('withHeaders', () => {
     server.close();
   });
 
-  const fetchWithExtraHeaders = withHeaders(fetch, () => ({ foo: 'bar', id: 'override' }));
+  const fetchWithExtraHeaders = withHeaders(fetch, () => ({ foo: 'foo', id: 'override' }));
 
   it('add headers', async () => {
     const server = createServer((req, res) => {
-      const response = req.headers.id;
+      const response = JSON.stringify({id: req.headers.id, bar: req.headers.bar, foo: req.headers.foo});
 
       res.writeHead(200);
       res.end(response);
@@ -36,8 +36,8 @@ describe('withHeaders', () => {
     await waitForServer(server);
 
     const { port } = server.address() as AddressInfo;
-    const res = await fetchWithExtraHeaders(`http://127.0.0.1:${port}`, { headers: { id: 'id' } });
-    await expect(res.text()).resolves.toBe('override');
+    const res = await fetchWithExtraHeaders(`http://127.0.0.1:${port}`, { headers: { id: 'id', bar: 'bar' } });
+    await expect(res.text()).resolves.toMatchInlineSnapshot(`"{"id":"override","bar":"bar","foo":"foo"}"`)
     server.close();
   });
 });
