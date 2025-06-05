4628120a0c8547b516d936ee3fbf417d3f408b75
Guy Segal <guysegal.il@gmail.com>
2025-06-05 11:34:58 +0300

    .

--- src/enhancers/withTimeout.ts
+++ src/enhancers/withTimeout.ts
@@ -9,33 +9,38 @@ export const withTimeout =
   (fetch: Fetch, options: TimeoutOptions): Fetch =>
   async (url, init) => {
     try {
+      const timeoutSignal = AbortSignal.timeout(options.requestTimeoutMs);
+      const signal = init?.signal ? AbortSignal.any([timeoutSignal, init.signal]) : timeoutSignal;
+      
       return await fetch(url, {
-        signal: AbortSignal.any([AbortSignal.timeout(options.requestTimeoutMs), ...(init?.signal ? [init.signal] : [])]),
+        signal,
         ...init,
       });
     } catch (error) {
+      if (!(error instanceof Error)) {
+        throw error;
+      }
+
       const errorUrl = typeof url === 'string' ? url : url instanceof URL ? url.toString() : url?.url;
-      if (error instanceof Error) {
-        switch (error.name) {
-          case 'AbortError': {
-            throw new FetchError({
-              message: 'abort requested',
-              url: errorUrl,
-              data: { timeoutOptions: options },
-            });
-          }
-          case 'TimeoutError': {
-            throw new FetchError({
-              message: 'request timeout',
-              url: errorUrl,
-              data: { timeoutOptions: options },
-            });
-          }
-          default: {
-            throw error;
-          }
+
+      switch (error.name) {
+        case 'AbortError': {
+          throw new FetchError({
+            message: 'abort requested',
+            url: errorUrl,
+            data: { timeoutOptions: options },
+          });
         }
-      } 
-      throw error;
+        case 'TimeoutError': {
+          throw new FetchError({
+            message: 'request timeout',
+            url: errorUrl,
+            data: { timeoutOptions: options },
+          });
+        }
+        default: {
+          throw error;
+        }
+      }
     }
   };
